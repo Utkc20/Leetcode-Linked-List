f81e38d26b7c971402a96c08055d0b0d4fa71740
Utkc20 <112210008+Utkc20@users.noreply.github.com>
2023-03-11 02:53:47 +0530

    You are given the heads of two sorted linked lists list1 and list2.  Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.  Return the head of the merged linked list.

---
+++ Merge Two Sorted Lists.cpp
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+
+private:
+    ListNode* solve(ListNode* first, ListNode* second) {
+
+    //if only one element is there in first list.
+    if(first->next==NULL){
+        first->next=second;
+        return first;
+    }
+    
+    
+     ListNode* curr1 = first;
+     ListNode* next1 = curr1 -> next;
+    
+    ListNode* curr2 = second;
+     ListNode* next2 = curr2 -> next;
+    
+    while(next1 != NULL && curr2 != NULL) {
+        //checking the value of 2 list node wheter it is smaller or not.
+        if( (curr2 -> val >= curr1 -> val ) 
+           && ( curr2 -> val <= next1 -> val)) {
+            //adding node in between.
+            curr1 -> next = curr2;
+            next2=curr2->next;
+            curr2 -> next = next1;
+            curr1 = curr2;
+            curr2 = next2;
+        }
+        
+        else {
+            curr1=next1;
+            next1=next1->next;
+
+            if(next1==NULL){
+                curr1->next=curr2;
+                return first;
+            }
+            
+        }
+        
+        
+    }
+    return first;
+    
+    
+}
+
+
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1== NULL)
+        return list2;
+    
+    if(list2 == NULL)
+        return list1;
+    
+    if(list1 -> val <= list2-> val ){
+        return solve(list1, list2);
+    }
+    else
+    {
+        return solve(list2, list1);
+    }
+    }
+};
